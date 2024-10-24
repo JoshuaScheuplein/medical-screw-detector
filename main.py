8588e642d39c24ed2481be6c89d8553a76f27d85
JoshuaScheuplein <joshua.scheuplein@gmail.com>
2024-10-24 18:36:57 +0200

    Bug fix

--- main.py
+++ main.py
@@ -129,17 +129,22 @@ def main(args):
     else:
         plugins = [SLURMEnvironment(requeue_signal=signal.SIGUSR1)]
 
-    trainer = Trainer(max_epochs=args.epochs,
-                      logger=logger,
-                      devices=1,
-                      num_nodes=1,
-                      default_root_dir=args.result_dir,
-                      log_every_n_steps=100, # How often to log within steps
-                      # callbacks=[checkpoint_val_callback, prediction_logging_callback], # Original Code
-                      callbacks=[checkpoint_train_callback, prediction_logging_callback], # Adapted Code
-                      plugins=plugins,
-                      enable_model_summary=True, # Enable detailed model summary (Additionally added)
-                      )
+    try: # Additionally added
+        trainer = Trainer(max_epochs=args.epochs,
+                        logger=logger,
+                        devices=1,
+                        num_nodes=1,
+                        default_root_dir=args.result_dir,
+                        log_every_n_steps=100, # How often to log within steps
+                        # callbacks=[checkpoint_val_callback, prediction_logging_callback], # Original Code
+                        callbacks=[checkpoint_train_callback, prediction_logging_callback], # Adapted Code
+                        plugins=plugins,
+                        enable_model_summary=True, # Enable detailed model summary (Additionally added)
+                        )
+    except Exception as e: # Additionally added
+        print(f"Error during training: {e}")
+        import traceback
+        traceback.print_exc()
 
     last_ckpt_file = args.result_dir + "/last.ckpt"
     if (args.checkpoint_file is None) and (os.path.isfile(last_ckpt_file)):
