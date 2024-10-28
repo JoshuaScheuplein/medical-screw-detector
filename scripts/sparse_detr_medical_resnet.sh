a588359b67d309d79f18b5c66266274e057f20d5
JoshuaScheuplein <joshua.scheuplein@gmail.com>
2024-10-28 12:22:40 +0100

    Bug fix

--- scripts/sparse_detr_medical_resnet.sh
+++ scripts/sparse_detr_medical_resnet.sh
@@ -90,7 +90,7 @@ srun python3 main.py \
   --lr 0.00004 \
   --lr_drop_epochs 40 \
   --lr_backbone 0.000004 \
-  --batch_size 3 \
+  --batch_size 2 \            # Default: 3
   --epochs 50 \
   --with_box_refine \
   --two_stage \
